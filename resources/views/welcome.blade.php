bc5c4083c5a386f97231471eff230b2b4790a38c
curder <curder@foxmail.com>
2019-08-20 11:05:26 +0800

    version 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,7 +82,7 @@
 
         <div class="content">
             <div class="title">
-                version 1
+                version 2
             </div>
             <div class="title m-b-md">
                 Laravel ❤️ CI/CD Deployer..
