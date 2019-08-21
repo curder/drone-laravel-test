fa82a0bcfdf059b0febd756de41ce16d20012aea
curder <curder@foxmail.com>
2019-08-21 09:57:25 +0800

    for release version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,7 +82,7 @@
 
         <div class="content">
             <div class="title">
-                version 5
+                version 6 for release
             </div>
             <div class="title m-b-md">
                 Laravel ❤️ CI/CD Deployer..
