667185d175cb0330fb76f5df88a9175694b4dbc3
curder <curder@foxmail.com>
2019-08-21 16:40:04 +0800

    修改部署分支的地址

--- config/deploy.php
+++ config/deploy.php
@@ -90,7 +90,7 @@ return [
 
     'options' => [
         'application' => env('APP_NAME', 'Laravel'),
-        'repository' => 'https://github.com/curder/drone-laravel-test.git',
+        'repository' => 'http://deployer:deployer_2018@git.avadev.cn:30600/curder/drone-laravel-test.git',
         'default_timeout' => null,
         'php_fpm_service' => 'php72-php-fpm',
 
